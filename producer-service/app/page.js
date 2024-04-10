2d1ef52bb41352c9189d82f11f9a440366649a03
Meowmaster <ashenoy64@gmail.com>
2024-04-10 20:41:31 +0530

    Added Ordering Component

--- producer-service/app/page.js
+++ producer-service/app/page.js
@@ -1,22 +1,81 @@
 "use client";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { Products } from "@/components/Products";
-
+import { CartOrder } from "@/components/CartOrder";
 export default function Home() {
-  const [products,setProducts] = useState([])
+  const [products, setProducts] = useState([])
+  const [cart, setCart] = useState([])
+  const [grandTotal, setGrandTotal] = useState(0)
   useEffect(() => {
     fetch('http://localhost:3000/api/products')
       .then(response => response.json())
-      .then(data => { setProducts(data)})
+      .then(data => { setProducts(data) })
   }, [])
+
+  const HandleButton = (ops, product) => {
+    if (ops == "add") {
+      let cart_product = cart.find((product) => product.id == product.id)
+      if (cart_product) {
+        if (product.quantity < cart_product.quantity) {
+          cart_product.quantity += 1
+          cart_product.total = cart_product.price * cart_product.quantity
+          setGrandTotal((prev)=>prev+cart_product.price)
+        }
+
+      }
+      else {
+        if (product.quantity > 0) {
+          cart_product = { "id": product.id, "name": product.name, "price": product.price, "quantity": 1, "total": product.price }
+          setCart([...cart, cart_product])
+          setGrandTotal((prev)=>prev+cart_product.price)
+        }
+      }
+    }
+    else if (ops == "remove") {
+      let cart_product = cart.find((product) => product.id == product.id)
+      if (cart_product) {
+        if (cart_product.quantity > 1) {
+          cart_product.quantity -= 1
+          cart_product.total = cart_product.price * cart_product.quantity
+          setGrandTotal((prev)=>prev-cart_product.price)
+        }
+        else {
+          let new_cart = cart.filter((product) => product.id != cart_product.id)
+          setCart(new_cart)
+          setGrandTotal((prev)=>prev-cart_product.price)
+        }
+      }
+    }
+    console.log(cart)
+  }
   return (
     <main className="w-screen">
       <div className="p-20 w-full flex flex-col gap-2 ">
         <h1 className="text-lg font-bold p-4">Products Available</h1>
-        <div className="w-full grid lg:grid-col-4 md:grid-cols-2 grid-cols-1 gap-12 justify-center items-center">
-          {products && products.map((product,index) => <Products key={index} product={product} /> ) }
+        <div className="w-full grid lg:grid-col-3 md:grid-cols-1 grid-cols-1 gap-12 justify-center items-center">
+          {products && products.map((product, index) => <Products key={index} product={product} button={HandleButton} />)}
         </div>
       </div>
+      {cart.length > 0 &&
+        <div className="w-full flex flex-col p-20 ">
+          <h1 className="text-lg font-bold p-4">Cart</h1>
+          <div className="w-full flex flex-col gap-12 justify-center items-center p-4">
+          <div className="grid grid-cols-4 text-center w-full text-base-content font-bold">
+            <div>Name</div>
+            <div>Price</div>
+            <div>Quantity</div>
+            <div>Total Price</div>
+          </div>
+            {cart.map((product, index) => <CartOrder key={index} product={product} />)}
+          </div>
+          <div className="w-full flex flex-row justify-between p-4">
+            <h1 className="text-lg font-bold">Grand Total</h1>
+            <h1 className="text-lg font-bold">{grandTotal}</h1>
+          </div>
+          <div className="flex flex-row-reverse">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => alert("Order Placed")}>Place Order</button>
+          </div>
+        </div>}
     </main>
   )
 }
--- producer-service/components/Products.jsx
+++ producer-service/components/Products.jsx
@@ -1,12 +1,12 @@
-export function Products({ product }) {
+export function Products({ product,button }) {
   return (
     <div className="card w-96 outline outline-1 mx-auto ">
       <div className="card-body">
         <h2 className="card-title">{product.name}</h2>
         <p>₹{product.price}</p>
         <div className="card-actions justify-end">
-          <button className="btn w-12 font-bold text-xl ">-</button>
-          <button className="btn w-12 font-bold text-xl ">+</button>
+          <button className="btn w-12 font-bold text-xl " onClick={()=>button("remove",product)}>-</button>
+          <button className="btn w-12 font-bold text-xl " onClick={()=>button("add",product)}>+</button>
         </div>
       </div>
     </div>
