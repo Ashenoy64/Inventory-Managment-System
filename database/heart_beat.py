aa77b9bbd514e19ea402b72dc43d97005d3deb6d
Meowmaster <ashenoy64@gmail.com>
2024-04-16 09:10:48 +0530

    Database

---
+++ database/heart_beat.py
@@ -0,0 +1,52 @@
+import pika
+import os
+import time
+import dotenv
+import json
+import datetime
+
+
+
+dotenv.load_dotenv()
+
+NODE_ID = os.getenv('NODE_ID')
+NODE_NAME = os.getenv('NODE_NAME')
+
+RABBITMQ_QUEUE = os.getenv('RABBITMQ_QUEUE')
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
+RABBITMQ_PORT = os.getenv('RABBITMQ_PORT')
+
+INTERVAL = os.getenv('INTERVAL')
+
+def send_heart_beat(mq_conection):
+    channel = mq_connection.channel()
+    channel.queue_declare(queue=RABBITMQ_QUEUE)
+
+    data = { 
+        "id": NODE_ID,
+        "node_name": NODE_NAME,
+        "checkpoint": str(datetime.datetime.now()),
+    }
+
+
+    try:
+        channel.basic_publish(exchange='', routing_key=RABBITMQ_QUEUE, body=json.dumps(data))
+        print("Sent ",data)
+    except Exception as e:
+        print(e)
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        mq_connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST,port=RABBITMQ_PORT))
+        print("Heart beat service started")
+        while True:
+            send_heart_beat(mq_connection)
+            time.sleep(int(INTERVAL))
+    except KeyboardInterrupt as e:
+        print("Exiting")
+        mq_connection.close()
+    except Exception as e:
+        print(e)
+        mq_connection.close()
